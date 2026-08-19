408ba6726b7df87cddd4df91ac43d01b89085d53
meganwilhelms <wilhelms.megan@gmail.com>
2026-08-19 09:01:03 -0500

    Create ProgReviewRetentionPersist.sql
    Calculates persistence and retention of new and transfer students. The report only looks at continued enrollment, not if a student graduated. Need to compare to a grad list in case the student graduated.

---
+++ ProgReviewRetentionPersist.sql
@@ -0,0 +1,40 @@
+WITH NewTransfers AS (
+SELECT st.Seq_Num, stsd.id_num, stsd.YR_CDE, st.AcadYear, stsd.TRM_CDE, st.Term, STSD.HRS_ENROLLED,
+	CASE WHEN MAJOR_1 in ('BAD', 'BADOL') THEN 'BAD'
+		WHEN MAJOR_1 in ('BSBAD', 'BSBAO') THEN 'BSBAD'
+		WHEN MAJOR_1 in ('CIS', 'CIT') THen 'CIS'
+		WHEN MAJOR_1 IN ('CJAAO', 'CJU') THEN 'CJU'
+		WHEN MAJOR_1 in ('BSCJO', 'BSCJU') THEN 'BSCJU'
+		WHEN MAJOR_1 IN ('GENA', 'GENOL') THEN 'GENA'
+		WHEN MAJOR_1 IN ('HSS', 'SWK') THEN 'SWK'
+		ELSE MAJOR_1
+		END AS MAJOR_1,
+	cv.CANDIDACY_TYPE
+from TmsEPrd.dbo.STUD_TERM_SUM_DIV stsd
+	LEFT JOIN TmsEPrd.dbo.candidacy_v cv ON stsd.ID_NUM=cv.ID_NUM AND stsd.YR_CDE=cv.YR_CDE AND stsd.TRM_CDE=cv.TRM_CDE and cv.CANDIDACY_TYPE in ('N', 'T')
+	LEFT JOIN TmsEPrd.[UTTC\mwilhelms].Sequential_Terms st ON stsd.YR_CDE=st.YR_CDE AND stsd.TRM_CDE=st.TRM_CDE
+where stsd.YR_CDE>2020  and HRS_ENROLLED>0 and stsd.TRM_CDE in ('30') and cv.CANDIDACY_TYPE is not null),
+
+allStudents AS (
+SELECT st.Seq_Num, stsd.id_num, stsd.YR_CDE, stsd.TRM_CDE, cv.CANDIDACY_TYPE
+from TmsEPrd.dbo.STUD_TERM_SUM_DIV stsd
+	LEFT JOIN TmsEPrd.dbo.candidacy_v cv ON stsd.ID_NUM=cv.ID_NUM AND stsd.YR_CDE=cv.YR_CDE AND stsd.TRM_CDE=cv.TRM_CDE 
+	LEFT JOIN TmsEPrd.[UTTC\mwilhelms].Sequential_Terms st ON stsd.YR_CDE=st.YR_CDE AND stsd.TRM_CDE=st.TRM_CDE
+where stsd.YR_CDE>2020  and HRS_ENROLLED>0 and stsd.TRM_CDE in ('30', '40') ),
+
+persistence AS (
+select a.ID_NUM, a.Seq_Num, b.Seq_Num AS Next_Seq_Num, a.yr_cde, a.trm_cde, a.candidacy_type,
+	CASE WHEN b.ID_NUM IS NOT NULL THEN 'Yes'
+	ELSE 'No' END AS Persisted
+FROM allStudents a 
+	LEFT JOIN allStudents b ON a.ID_NUM=b.ID_NUM AND b.Seq_Num=a.Seq_Num+1),
+retention AS (
+select a.ID_NUM, a.Seq_Num, b.Seq_Num AS Next_Seq_Num, a.yr_cde, a.trm_cde, a.candidacy_type,
+	CASE WHEN b.ID_NUM IS NOT NULL THEN 'Yes'
+	ELSE 'No' END AS Retained
+FROM allStudents a 
+	LEFT JOIN allStudents b ON a.ID_NUM=b.ID_NUM AND b.Seq_Num=a.Seq_Num+3)
+select nt.*, Persisted, Retained
+FROM NewTransfers nt 
+	LEFT JOIN persistence p ON nt.ID_NUM=p.ID_NUM AND nt.Seq_Num=p.Seq_Num
+	LEFT JOIN retention r ON nt.ID_NUM=r.ID_NUM AND nt.Seq_Num=r.Seq_Num
